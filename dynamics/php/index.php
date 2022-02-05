ace30ba6f01f6a9ebb81e7b84b82d38408853444
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2022-02-04 21:31:21 -0600

    Base de datos

--- dynamics/php/index.php
+++ dynamics/php/index.php
@@ -1,9 +1,26 @@
 <?php
     
-    $se = "Usuario: ";
+    $se = "usuario";
     echo "Hola  ".$se;
+   
+
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
     if(isset($_POST['usr']) && isset($_POST['pass'])){
         $usr=$_POST['usr'];
         echo "Tu nombre es: ".$usr;
     }
+
 ?>
