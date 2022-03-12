1f67343aa3b8bc77fc7fc27f326e64aa83675017
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2022-03-11 18:40:21 -0600

    Ya esta info de cliente

--- dynamics/php/infoCliente.php
+++ dynamics/php/infoCliente.php
@@ -13,7 +13,7 @@
     }
 
     
-    if(isset($_POST['terminar'])){unset($_SESSION['client']); header('Location: ../../templates/empleado.html');}
+    if(isset($_POST['terminar'])){unset($_SESSION['client']); header('Location: ../..');}
     
 
     if(isset($_SESSION['client'])){
@@ -24,31 +24,49 @@
             echo "Hola ".$row[0]." ".$row[1];
             $pets = $row[2];    
             $tmp = explode(",",$pets);
+            echo "<table>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Edad</th>
+                    <th>Tipo</th>
+                    <th>Veterinario</th>
+                    <th>Diagnostico</th>
+                    <th>Procedimientos</th>
+                </tr>";
                 for ($i=0; $i < count($tmp); $i++) { 
-                   $sql = "SELECT * FROM Mascota WHERE ID ='". $tmp[$i]. "'";
+                    $sql = "SELECT * FROM Mascota WHERE ID ='". $tmp[$i]. "'";
                     $result = mysqli_query($conn, $sql);
                     while($row3 = mysqli_fetch_array($result)){
-                        echo "<table>
-                            <tr>
-                                <th>Nombre</th>
-                                <th>Edad</th>
-                                <th>Tipo</th>
-                                <th>Veterinario</th>
-                                <th>Diagnostico</th>
-                                <th>Procedimientos</th>
-                            </tr>";
                         echo "<tr>";
                         echo "<td>".$row3[2]."</td>";
                         echo "<td>".$row3[3]."</td>";
                         echo "<td>".$row3[4]."</td>";
-                        echo "<td>".$row3[5]."</td>";
+                        //echo "<td>".$row3[5]."</td>";
+                        $sql = "SELECT Nombres,Apellidos FROM Empleado WHERE ID ='".$row3[5]. "'";
+                        $result = mysqli_query($conn, $sql);
+                        if($row2 = mysqli_fetch_array($result)){
+                            echo "<td>".$row2[0]." ".$row2[1]."</td>";
+                        }else{
+                            echo "<td>No se encontro</td>";
+                        }
+
                         echo "<td>".$row3[6]."</td>";
-                        echo "<td>".$row3[7]."</td>";
+                        //echo "<td>".$row3[7]."</td>";
+                        $tmpP = explode(",",$row3[7]);
+                        for($j=0; $j < count($tmpP); $j++){
+                            $sql = "SELECT Nombre, Descripcion FROM Procedimiento WHERE ID ='".$tmpP[$j]. "'";
+                            $result = mysqli_query($conn, $sql);
+                            if($row2 = mysqli_fetch_array($result)){
+                                echo "<td>".$row2[0].". ".$row2[1]."<br></td>";
+                            }else{
+                                echo "<td></td>";
+                            }
+                        }
                         echo "</tr>";
                     }
+                }
             }
-        }
-        
+            echo "<form action='infoCliente.php' method='POST'><button name='terminar'>Terminar</button></form>";
     }else{
         echo "Inicie sesión con el correo registrado";
     }
